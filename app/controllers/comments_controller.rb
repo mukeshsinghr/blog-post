69412a9e76f17942cf00011b81ffe9a321b6fe27
Mukesh Singh Rathaur <mukesh.rathaur@oracle.com>
2015-12-05 08:36:55 +0530

    removed comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,6 @@
 class CommentsController  < ApplicationController
 
   def create
-    #    puts params
     @post = Post.find params[:post_id]
     @post.comments.create(comments_attributes)
     redirect_to @post
